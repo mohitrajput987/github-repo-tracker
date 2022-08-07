40bbc4ef4512836c007ef0f19a3e4b8d54d819e2
Mohit Rajput <mohitrajput987@gmail.com>
2022-08-07 15:49:19 +0530

    refactor: align base files at proper place

--- app/src/main/java/com/otb/githubtracker/common/base/BaseActivity.kt
+++ app/src/main/java/com/otb/githubtracker/common/base/BaseActivity.kt
@@ -1,12 +1,9 @@
-package com.otb.githubtracker.common
+package com.otb.githubtracker.common.base
 
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewbinding.ViewBinding
-import com.otb.githubtracker.common.base.DisplaysErrorMessage
-import com.otb.githubtracker.common.base.DisplaysLoadingSpinner
 
 /**
  * Created by Mohit Rajput on 06/08/22.
@@ -14,12 +11,10 @@ import com.otb.githubtracker.common.base.DisplaysLoadingSpinner
  * binding.
  * Child activity need to implement inflateLayout() with specific binding.
  */
-abstract class BaseActivity<VB : ViewBinding> : AppCompatActivity(), DisplaysLoadingSpinner,
-    DisplaysErrorMessage {
+abstract class BaseActivity<VB : ViewBinding> : AppCompatActivity() {
 
     private var _binding: ViewBinding? = null
     abstract fun inflateLayout(layoutInflater: LayoutInflater): VB
-    override lateinit var loadingSpinner: LoadingSpinner
 
     @Suppress("UNCHECKED_CAST")
     protected val binding: VB
@@ -29,7 +24,6 @@ abstract class BaseActivity<VB : ViewBinding> : AppCompatActivity(), DisplaysLoa
         super.onCreate(savedInstanceState)
         _binding = inflateLayout(layoutInflater)
         setContentView(requireNotNull(_binding).root)
-        loadingSpinner = LoadingSpinner(this)
         setupView()
     }
 
@@ -38,10 +32,5 @@ abstract class BaseActivity<VB : ViewBinding> : AppCompatActivity(), DisplaysLoa
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
-    }
-
-    override fun showErrorMessage(errorMessage: String) {
-        dismissLoadingSpinner()
-        Toast.makeText(applicationContext, errorMessage, Toast.LENGTH_LONG).show()
     }
 }
